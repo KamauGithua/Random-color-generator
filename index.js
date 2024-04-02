26bed35cba2b1ac777ad50f508972fa01e0f4290
kamau <githuakamau99@gmail.com>
2024-04-02 18:13:50 +0300

    a random color genarator

---
+++ index.js
@@ -0,0 +1,3 @@
+function changeColor(){
+  document.body.style.backgroundColor= 'rgb(' + Math.round(Math.random() * 256) + ',' + Math.round(Math.random() * 256) + ',' + Math.round(Math.random() * 256) + ')';
+}
